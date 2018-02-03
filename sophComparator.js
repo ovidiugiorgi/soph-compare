c9d546cdd95a8ca27f35c8bff7a833f6063877a0
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2018-02-03 03:51:41 +0200

    Add direction and converter features

--- sophComparator.js
+++ sophComparator.js
@@ -8,16 +8,33 @@ const compare = (a, b) => {
   return 0;
 };
 
+const inverseCompare = (a, b) => -1 * compare(a, b);
+
+const getCompareFunction = (direction) =>
+  direction === 'DESC' ? inverseCompare : compare;
+
+const getValue = (a, orderItem) =>
+  orderItem.converter
+    ? orderItem.converter(a[orderItem.prop])
+    : a[orderItem.prop];
+
 const sophComparator = (orderList) => {
   if (!orderList || orderList.length === 0) {
     return compare;
   }
 
+  if (orderList.length === 1 && orderList[0].direction) {
+    return getCompareFunction(orderList[0].direction);
+  }
+
   return (a, b) => {
     for (let index = 0; index < orderList.length; index += 1) {
       const orderItem = orderList[index];
-      const { prop } = orderItem;
-      const compareResult = compare(a[prop], b[prop]);
+      const compareFunction = getCompareFunction(orderItem.direction);
+      const compareResult = compareFunction(
+        getValue(a, orderItem),
+        getValue(b, orderItem)
+      );
 
       if (compareResult) {
         return compareResult;
--- sophComparator.test.js
+++ sophComparator.test.js
@@ -2,7 +2,6 @@ const sophComparator = require('./sophComparator');
 
 it('returns comparator if no order is provided - numbers', () => {
   const arr = [5, 4, -2, 12, 11, 4];
-
   const arrSorted = [...arr].sort((a, b) => a - b);
 
   expect(arr.sort(sophComparator())).toEqual(arrSorted);
@@ -10,7 +9,6 @@ it('returns comparator if no order is provided - numbers', () => {
 
 it('returns comparator if no order is provided - strings', () => {
   const arr = ['flower', 'water', 'conditioner', 'water'];
-
   const arrSorted = [...arr].sort((a, b) => a > b);
 
   expect(arr.sort(sophComparator())).toEqual(arrSorted);
@@ -18,7 +16,6 @@ it('returns comparator if no order is provided - strings', () => {
 
 it('returns comparator if an empty order list is provided', () => {
   const arr = [5, 4, -2, 12, 11, 4];
-
   const arrSorted = [...arr].sort((a, b) => a - b);
 
   expect(arr.sort(sophComparator([]))).toEqual(arrSorted);
@@ -32,7 +29,7 @@ it('returns comparator when order list has basic items', () => {
     },
     {
       price: 14,
-      name: 'bread',
+      name: 'avocado',
     },
     {
       price: 14,
@@ -52,11 +49,151 @@ it('returns comparator when order list has basic items', () => {
   expect(arr.sort(sophComparator(cfg))).toEqual([
     {
       price: 14,
+      name: 'avocado',
+    },
+    {
+      price: 14,
       name: 'banana',
+    },
+    {
+      price: 32,
+      name: 'soy sauce packet',
+    },
+  ]);
+});
+
+it('returns comparator when order list contains only DESC direction', () => {
+  const arr = [4, 2, 5, 8, 10];
+  const arrSorted = [...arr].sort((a, b) => a < b);
+
+  const cfg = [
+    {
+      direction: 'DESC',
+    },
+  ];
+
+  expect(arr.sort(sophComparator(cfg))).toEqual(arrSorted);
+});
+
+it('returns comparator when order items have DESC direction', () => {
+  const arr = [
+    {
+      price: 32,
+      name: 'soy sauce packet',
     },
     {
       price: 14,
       name: 'bread',
+    },
+    {
+      price: 14,
+      name: 'avocado',
+    },
+  ];
+
+  const cfg = [
+    {
+      prop: 'price',
+    },
+    {
+      prop: 'name',
+      direction: 'DESC',
+    },
+  ];
+
+  expect(arr.sort(sophComparator(cfg))).toEqual([
+    {
+      price: 14,
+      name: 'bread',
+    },
+    {
+      price: 14,
+      name: 'avocado',
+    },
+    {
+      price: 32,
+      name: 'soy sauce packet',
+    },
+  ]);
+});
+
+it('returns comparator when order items have a converter function attached: convert to boolean', () => {
+  const arr = [
+    {
+      price: 32,
+      name: 'soy sauce packet',
+    },
+    {
+      price: 48,
+      name: 'bread',
+    },
+    {
+      price: 15,
+      name: 'avocado',
+    },
+  ];
+
+  const cfg = [
+    {
+      prop: 'price',
+      converter: (a) => a % 2 === 0,
+      direction: 'DESC',
+    },
+    {
+      prop: 'name',
+    },
+  ];
+
+  expect(arr.sort(sophComparator(cfg))).toEqual([
+    {
+      price: 48,
+      name: 'bread',
+    },
+    {
+      price: 32,
+      name: 'soy sauce packet',
+    },
+    {
+      price: 15,
+      name: 'avocado',
+    },
+  ]);
+});
+
+it('returns comparator when order items have converter from string to number', () => {
+  const arr = [
+    {
+      price: 32,
+      name: 'soy sauce packet',
+    },
+    {
+      price: 48,
+      name: 'bread',
+    },
+    {
+      price: 15,
+      name: 'avocado',
+    },
+  ];
+
+  const cfg = [
+    {
+      prop: 'name',
+      converter: (a) => a.length,
+    },
+    {
+      prop: 'price',
+    },
+  ];
+
+  expect(arr.sort(sophComparator(cfg))).toEqual([
+    {
+      price: 48,
+      name: 'bread',
+    },
+    {
+      price: 15,
+      name: 'avocado',
     },
     {
       price: 32,
