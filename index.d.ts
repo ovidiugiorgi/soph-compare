b835ede4f024056471165f927029d2e9cdb28665
Ovidiu Giorgi <ovidiu.giorgi@tremend.com>
2018-02-06 14:22:38 +0200

    Fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 export = sophCompare;
 
-declare function sophCompare(orderList: OrderItem[]): (a: T, b: T) => number;
+declare function sophCompare(orderList: sophCompare.OrderItem[]): (a: any, b: any) => number;
 
 declare namespace sophCompare {
   export interface OrderItem {
     prop?: string | number;
     descending?: boolean;
-    converterFn?: (a: T) => T;
-    compareFn?: (a: T, b: T) => number;
+    converterFn?: (a: any) => any;
+    compareFn?: (a: any, b: any) => number;
   }
 }
